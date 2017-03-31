37e2a365b8774d1dbd6b5901c3a8b41f6cba4a6d
Craeg Strong <cstrong@arielpartners.com>
2017-03-31 13:04:50 -0400

    renamed webapp

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ForecastsModule } from './forecasts/forecasts.module';
+import { HotelsModule } from './hotels/hotels.module';
+import { OfflineUnitsModule } from './offline-units/offline-units.module';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app.routing';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    SidebarComponent
+  ],
+  imports: [
+    NgbModule.forRoot(),
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ForecastsModule,
+    OfflineUnitsModule,
+    HotelsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cd-sidebar',
+  templateUrl: 'sidebar.component.html',
+  styleUrls: ['sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  agencyTitle = 'DSS';
+  appShTitle = 'CapDash';
+  appTitle = 'Shelter Capacity & Planning';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
