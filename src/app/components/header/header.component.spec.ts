f64130cad9d78a33a7d22bc05989e7e1e4a17f6d
milocosmopolitan <milocosmopolitan@gmail.com>
2017-05-15 09:38:39 -0400

    unit test for function in header components

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,17 +1,37 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component } from '@angular/core';
+import { async, fakeAsync, ComponentFixture, TestBed, tick } from '@angular/core/testing';
+import { Router } from "@angular/router";
 import { RouterTestingModule } from '@angular/router/testing';
+import { Location } from "@angular/common";
+
+import { MockComponent } from 'ng2-mock-component';
 import { NgReduxTestingModule, MockNgRedux } from '@angular-redux/store/testing';
+
+import { AppComponent } from '../../app.component';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
+
+  @Component({
+    template: ''
+  })
+  class DummyComponent { }
+
+  let location: Location;
+  let router: Router;
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
+      declarations: [
+        DummyComponent,
+        HeaderComponent
+      ],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'login', component: DummyComponent }
+        ]),
         NgReduxTestingModule,
       ],
     })
@@ -20,12 +40,67 @@ describe('HeaderComponent', () => {
   }));
 
   beforeEach(() => {
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // fixture.detectChanges();
+
+    router.initialNavigation();
   });
 
-  it('should create', () => {
+  it('should create', async(() => {
     expect(component).toBeTruthy();
+  }));
+
+  describe('logout()', () => {
+    let button;
+    beforeEach(() => {
+      spyOn(component, 'logout');
+      // spyOn(component, 'toggleRadio');
+      button = fixture.debugElement.nativeElement.querySelector('.logout');
+      button.click();
+      fixture.detectChanges();
+    });
+
+    it('should have been called when button is clicked', () => {
+      fixture.whenStable().then(() => {
+        expect(component.logout).toHaveBeenCalled();
+        // expect(component.toggleRadio).toHaveBeenCalled();
+      })
+    });
+
+    it('should redirects to /login route', () => {
+      let href = button.getAttribute('href');
+      expect(href).toBe('/login');
+    });
+  });
+
+  describe('toggleRadio()', () => {
+
+    let button;
+    beforeEach(() => {
+      spyOn(component, 'toggleRadio');
+      fixture.detectChanges();
+      button = fixture.debugElement.nativeElement.querySelector('#dropdown-menu-lg-checkbox');
+      button.click();
+    });
+
+    it('should have been called when radio button is clicked', async(() => {
+      fixture.whenStable().then(() => {
+        expect(component.toggleRadio).toHaveBeenCalled();
+      })
+    }));
+
+    it('should check when radio button is clicked', () => {
+      expect(button.checked).toBe(true);
+    });
+
+    it('should uncheck when other readio button is clicked', () => {
+      fixture.debugElement.nativeElement.querySelector('#user-checkbox').click();
+      expect(button.checked).toBe(false);
+    });
+
   });
 });
