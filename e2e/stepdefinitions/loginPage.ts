8d9c6ed7ab21270ac078526b33da5b35ba0c7ad2
Graig Janssen <graig.janssen@gmail.com>
2017-05-22 10:56:29 -0400

    Switch to chai from jasmine for e2e

--- e2e/stepdefinitions/loginPage.ts
+++ e2e/stepdefinitions/loginPage.ts
@@ -1,7 +1,10 @@
 import { browser } from 'protractor';
 import { Capdash2Page } from '../app.po';
 import { defineSupportCode } from 'cucumber';
-import {} from 'jasmine';
+import { expect } from 'chai';
+
+// let chai = require('chai').use(require('chai-as-promised'));
+// let expect = chai.expect;
 
 defineSupportCode(function ({Given}) {
   let page: Capdash2Page = new Capdash2Page();
@@ -9,7 +12,7 @@ defineSupportCode(function ({Given}) {
   Given(/^I am on login page$/, () => {
     page.navigateTo();
     return page.getTitle().then(title => {
-      expect(title).toEqual("something");
+      expect(title).to.eventually.equal("something");
     });
   });
 })
