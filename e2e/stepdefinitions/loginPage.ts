dd89d0d9db678c82e5da9107231afe167400fcef
Graig Janssen <graig.janssen@gmail.com>
2017-05-22 11:16:59 -0400

    Fix bug in step definition, test passes

--- e2e/stepdefinitions/loginPage.ts
+++ e2e/stepdefinitions/loginPage.ts
@@ -3,16 +3,13 @@ import { Capdash2Page } from '../app.po';
 import { defineSupportCode } from 'cucumber';
 import { expect } from 'chai';
 
-// let chai = require('chai').use(require('chai-as-promised'));
-// let expect = chai.expect;
-
 defineSupportCode(function ({Given}) {
   let page: Capdash2Page = new Capdash2Page();
 
   Given(/^I am on login page$/, () => {
     page.navigateTo();
     return page.getTitle().then(title => {
-      expect(title).to.eventually.equal("something");
+      expect(title).to.equal("Shelter Capacity Dashboard");
     });
   });
 })
