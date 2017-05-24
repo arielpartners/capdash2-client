37676c5ccab5424a90dc3c6e652fe60131e36139
Charles Fulnecky <charles.fulnecky@hbo.com>
2017-05-24 13:15:31 -0400

    Make steps match.

--- e2e/feature/authentication/login/step_definitions/loginFeature_steps.ts
+++ e2e/feature/authentication/login/step_definitions/loginFeature_steps.ts
@@ -9,23 +9,28 @@ defineSupportCode(({Given, When, Then}) => {
   let page: LoginPage = new LoginPage();
   let app: Capdash2Page = new Capdash2Page();
 
-  Given('The user navigates to the CapDash2 Homepage', () => {
+  Given('the user navigates to the CapDash2 homepage', () => {
     page.navigateTo();
     return page.getTitle().then(title => {
-      expect(title).to.eventually.equal("Shelter Capacity Dashboard");
+      expect(title).to.equal('Shelter Capacity Dashboard');
     });
   });
 
-  When('The user enters a valid email and password, then clicks sign in button', () => {
+  When('the user enters a valid email address and password', () => {
     page.enterEmail('sample_user@dhs.nyc.gov');
     page.enterPassword('password');
     page.signIn();
   });
 
-  Then('The user should see their personalized dashboard', () => {
+  When('the user clicks the sign in button', () => {
+    // Write code here that turns the phrase above into concrete actions
+
+  });
+
+  Then('the user should see their personalized dashboard', () => {
     return app.getTextByCss('page-header').then(text => {
       expect(text).to.eventually.equal('Welcome to Capacity Dashboard!');
     });
   });
 
-})
+});
