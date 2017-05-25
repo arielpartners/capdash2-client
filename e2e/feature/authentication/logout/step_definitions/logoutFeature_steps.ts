b845b6ed769bd3b8f19cf3c156bcbda345e760a0
Graig Janssen <graig.janssen@gmail.com>
2017-05-25 16:36:04 -0400

    Add Given to logout step def, login method for login PO

--- e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
+++ e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
@@ -6,5 +6,14 @@ import { defineSupportCode } from 'cucumber';
 import { expect } from 'chai';
 
 defineSupportCode(({Given, Then, When}) => {
+  let loginPage: LoginPage = new LoginPage();
+
+  Given('the user is logged in', () => {
+    loginPage.login('sample_user@dhs.nyc.gov', 'password').then(() => {
+      browser.getCurrentUrl().then(url => {
+        expect(/login/.test(url)).not.to.equal(true);
+      });
+    });
+  });
 
 });
