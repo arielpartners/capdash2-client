824f64ab627cdcdb5ea01473c11898c8a81e0353
Graig Janssen <graig.janssen@gmail.com>
2017-05-25 16:09:08 -0400

    Add logout feature, step def boilerplate

---
+++ e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
@@ -0,0 +1,10 @@
+import { browser} from 'protractor';
+import { LoginPage } from '../../../../page-objects/login.po';
+
+import { defineSupportCode } from 'cucumber';
+
+import { expect } from 'chai';
+
+defineSupportCode(({Given, Then, When}) => {
+
+});
