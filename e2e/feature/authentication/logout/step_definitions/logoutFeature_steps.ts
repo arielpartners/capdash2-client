a9f3561795399ac1f1bd4a22928ccda8ed5fc7c4
Graig Janssen <graig.janssen@gmail.com>
2017-05-25 16:50:55 -0400

    Add Header page object, import in logout step def

--- e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
+++ e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
@@ -1,5 +1,6 @@
 import { browser} from 'protractor';
 import { LoginPage } from '../../../../page-objects/login.po';
+import { HeaderPage } from '../../../../page-objects/header.po';
 
 import { defineSupportCode } from 'cucumber';
 
@@ -7,7 +8,8 @@ import { expect } from 'chai';
 
 defineSupportCode(({Given, Then, When}) => {
   let loginPage: LoginPage = new LoginPage();
-
+  let header: HeaderPage = new HeaderPage();
+  
   Given('the user is logged in', () => {
     loginPage.login('sample_user@dhs.nyc.gov', 'password').then(() => {
       browser.getCurrentUrl().then(url => {
@@ -15,5 +17,9 @@ defineSupportCode(({Given, Then, When}) => {
       });
     });
   });
+
+  When('the user selects the user dropdown menu', () => {
+
+  })
 
 });
