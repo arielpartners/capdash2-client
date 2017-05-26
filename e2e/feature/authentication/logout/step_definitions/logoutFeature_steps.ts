d5479677fc9a42e23ec07c8c06ce0066309c42e5
Graig Janssen <graig.janssen@gmail.com>
2017-05-26 13:22:09 -0400

    Add logout feature test

--- e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
+++ e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
@@ -1,5 +1,4 @@
-import { browser} from 'protractor';
-import { LoginPage } from '../../../../page-objects/login.po';
+import { browser, element, by } from 'protractor';
 import { HeaderPage } from '../../../../page-objects/header.po';
 
 import { defineSupportCode } from 'cucumber';
@@ -7,19 +6,33 @@ import { defineSupportCode } from 'cucumber';
 import { expect } from 'chai';
 
 defineSupportCode(({Given, Then, When}) => {
-  let loginPage: LoginPage = new LoginPage();
   let header: HeaderPage = new HeaderPage();
 
   Given('the user is logged in', () => {
-    loginPage.login('sample_user@dhs.nyc.gov', 'password').then(() => {
-      browser.getCurrentUrl().then(url => {
-        expect(/login/.test(url)).not.to.equal(true);
-      });
+    header.navigateTo();
+    return browser.getCurrentUrl().then(url => {
+      expect(/login/.test(url)).not.to.equal(true);
     });
   });
 
   When('the user selects the user dropdown menu', () => {
+    return header.clickUserDropdown();
+  });
 
-  })
+  Then('the user should see the option to log out', () => {
+    return header.logoutEl.isDisplayed().then(displayed => {
+      expect(displayed).to.equal(true);
+    });
+  });
+
+  When('the user selects log out', () => {
+    return header.clickLogout();
+  });
+
+  Then('the user should see the login page', () => {
+    return browser.getCurrentUrl().then(url => {
+      expect(/login/.test(url)).to.equal(true);
+    });
+  });
 
 });
--- e2e/page-objects/header.po.ts
+++ e2e/page-objects/header.po.ts
@@ -1,5 +1,19 @@
 import { browser, element, by } from 'protractor';
 
 export class HeaderPage {
-  
+  userDropdownEl = element(by.css('.navbar-user'));
+  logoutEl = element(by.css('.logout'));
+
+  clickUserDropdown () {
+    return this.userDropdownEl.click();
+  }
+
+  clickLogout () {
+    return this.logoutEl.click();
+  }
+
+  navigateTo () {
+    return browser.get('/');
+  }
+
 }
