58cfdd10dbac798372cbef85cb3ebfa6eed0874c
Graig Janssen <graig.janssen@gmail.com>
2017-05-26 10:52:22 -0400

    begin logout step def

--- e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
+++ e2e/feature/authentication/logout/step_definitions/logoutFeature_steps.ts
@@ -9,7 +9,7 @@ import { expect } from 'chai';
 defineSupportCode(({Given, Then, When}) => {
   let loginPage: LoginPage = new LoginPage();
   let header: HeaderPage = new HeaderPage();
-  
+
   Given('the user is logged in', () => {
     loginPage.login('sample_user@dhs.nyc.gov', 'password').then(() => {
       browser.getCurrentUrl().then(url => {
