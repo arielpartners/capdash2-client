ecee24d3d807570d58cefb70229a5ca6826e3bd2
Graig Janssen <graig.janssen@gmail.com>
2017-06-01 10:41:44 -0400

    E2E test for mega menu dropdown

---
+++ e2e/feature/dashboard/header/step_definitions/headerFeature_steps.ts
@@ -0,0 +1,29 @@
+import { browser, element, by } from 'protractor';
+import { HeaderPage } from '../../../../page-objects/header.po';
+import { E2EHelpers } from '../../../../support/e2eHelpers';
+
+import { defineSupportCode } from 'cucumber';
+
+import { expect } from 'chai';
+
+defineSupportCode(({Given, When, Then}) => {
+  const header: HeaderPage = new HeaderPage();
+
+  // Given the user is logged in: See logoutFeature_steps step 1
+
+  When(/the user selects the Main Menu dropdown/, () => {
+    return header.clickMegaMenu();
+  });
+
+  Then('the user should see the mega menu', () => {
+    return header.dropdownHeader.isPresent().then(present => {
+      expect(present).to.equal(true);
+    });
+  });
+
+  Then('the user should not see the mega menu', () => {
+    return header.dropdownHeader.isPresent().then(present => {
+      expect(present).to.equal(false);
+    });
+  });
+});
--- e2e/page-objects/header.po.ts
+++ e2e/page-objects/header.po.ts
@@ -1,8 +1,14 @@
 import { browser, element, by } from 'protractor';
 
 export class HeaderPage {
+  megaMenuDropdownEl = element(by.css('.dropdown-lg'));
+  dropdownHeader = element(by.css('.dropdown-header'));
   userDropdownEl = element(by.css('.navbar-user'));
   logoutEl = element(by.css('.logout'));
+
+  clickMegaMenu () {
+    return this.megaMenuDropdownEl.click();
+  }
 
   clickUserDropdown () {
     return this.userDropdownEl.click();
