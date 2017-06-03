862640329baf7bd60f16611fd2f386028b4f07d3
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-02 20:58:24 -0400

    passdown classnames

--- src/app/components/menu/menu-button/menu-button.component.ts
+++ src/app/components/menu/menu-button/menu-button.component.ts
@@ -3,7 +3,17 @@ import {MenuService} from '../../../services/menu/menu.service';
 
 @Component({
   selector: 'cd-menu-button',
-  templateUrl: './menu-button.component.html',
+  template: `
+    <label  [for]="inputId"
+            [ngClass]="_classList"
+            class="dropdown-toggle text-white">
+      <ng-content></ng-content>
+    </label>
+    <input type="radio" class="radio-hack"
+           [id]="inputId"
+           [name]="name"
+           (click)="onInputClick($event)">
+  `,
   styleUrls: ['./menu-button.component.less']
 })
 
@@ -14,6 +24,8 @@ export class MenuButtonComponent {
   @Input() id: string;
   @Input() name: string;
 
+  _classList: any = {};
+
   constructor(public menu: MenuService) {}
 
   get inputId(): string {
@@ -22,5 +34,15 @@ export class MenuButtonComponent {
 
   onInputClick(e): void {
     this.menu.toggleDropdown(e);
+  }
+
+  @Input('className')
+  set classList(classes: string) {
+    this._classList = classes.split(' ').reduce((obj: any, className: string) => {
+      obj[className] = true;
+      return obj;
+    }, {});
+    // set given ID to classList
+    this._classList[this.id] = true;
   }
 }
