2957c9773c7a7636911eee4f4026ec18bbd2007f
milocosmopolitan <milocosmopolitan@gmail.com>
2017-06-09 14:39:26 -0400

    fix(MenuItemComponent): css ommisions and view encapsulation configure

--- src/app/components/menu/menu-item/menu-item.component.ts
+++ src/app/components/menu/menu-item/menu-item.component.ts
@@ -7,27 +7,16 @@ import {
 @Component({
   selector: 'cd-menu-item',
   template: `
-    <li [ngClass]="_classList">
+    <ng-container>
       <a [href]="link" cdCloseToggle>
         <ng-content></ng-content>
       </a>
-    </li>
+    </ng-container>
   `,
   styleUrls: ['./menu-item.component.less'],
   encapsulation: ViewEncapsulation.None,
 })
 
 export class MenuItemComponent {
-
   @Input() link: string;
-
-  _classList: any = {};
-
-  @Input('className')
-  set classList(classes: string) {
-    this._classList = classes.split(' ').reduce((obj: any, className: string) => {
-      obj[className] = true;
-      return obj;
-    }, {});
-  }
 }
